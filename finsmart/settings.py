aade9f752591422a3719440597e80f597275de65
Priyanka <priyankamurugan232006@gmail.com>
2026-07-11 20:59:06 +0530

    Fix default environment variables

--- finsmart/settings.py
+++ finsmart/settings.py
@@ -18,14 +18,13 @@ SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="*").split(",")
 
-CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS").split(",")
+CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", default="").split(",")
 
-CORS_ALLOWED_ORIGINS = config("CORS_ALLOWED_ORIGINS").split(",")
+CORS_ALLOWED_ORIGINS = config("CORS_ALLOWED_ORIGINS", default="").split(",")
 
 CORS_ALLOW_CREDENTIALS = True
-ALLOWED_HOSTS = ["*"]
 # --------------------------------------------------
 # Applications
 # --------------------------------------------------
